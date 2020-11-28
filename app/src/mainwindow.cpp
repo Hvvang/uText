08ebd54cc131738ef85a4641af984143cfb7de37
Hwang <44523008+Hvvang@users.noreply.github.com>
2020-11-28 04:26:29 +0200

    Add menu bar

--- app/src/mainwindow.cpp
+++ app/src/mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "ui_mainwindow.h"
 #include "mainwindow.h"
+#include "FileObserver.h"
+//#include <QFileSystemModel>
 
 
 MainWindow::MainWindow(QString sPath, QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    ui->treeView->setRootPath(sPath);
+    ui->textEdit->setHidden(true);
 }
 
 MainWindow::~MainWindow() {
--- app/src/mainwindow.h
+++ app/src/mainwindow.h
@@ -18,6 +18,7 @@ public:
     ~MainWindow();
 
 
+
 private:
     Ui::MainWindow *ui;
 };
